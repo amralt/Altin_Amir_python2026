f34c9e0c3ffee6f71fd02d4f2b671464c822f132
amralt <amiralt@gmail.com>
2026-05-16 23:53:03 +0700

    сделаны обработчики балла по группе и шаблон

--- parser.py
+++ parser.py
@@ -1,21 +1,34 @@
 import pandas as pd
 
 
-hw1_df = pd.read_csv('python-2025 - big-hw-01.csv')
-hw2_df = pd.read_csv('python-2025 - big-hw-02.csv')
+hw_01_df = pd.read_csv('python-2025 - big-hw-01.csv')
+hw_02_df = pd.read_csv('python-2025 - big-hw-02.csv')
 
 def get_df_by_hw_name(hw_name: str) -> pd.DataFrame:
-    if hw_name == 'hw1':
-        return hw1_df
-    elif hw_name == 'hw2':
-        return hw2_df
+    if hw_name == 'hw-01':
+        return hw_01_df
+    elif hw_name == 'hw-02':
+        return hw_02_df
     else:
         raise ValueError(f"Unknown homework name: {hw_name}")
 
-def get_names():
-    names_1 = hw1_df['Unnamed: 0'].tolist()
-    names_2 = hw2_df['Unnamed: 0'].tolist()
+def get_names() -> list[str]:
+    names_1 = hw_01_df['Unnamed: 0'].tolist()
+    names_2 = hw_02_df['Unnamed: 0'].tolist()
     names = set(names_1) | set(names_2)
+    return list(names)
+
+
+def get_group_names(group_id: int) -> list[str]:
+    names = set()
+    for tup in hw_01_df.itertuples():
+        if int(tup[2]) == group_id:
+            names.add(tup[1])
+
+    for tup in hw_02_df.itertuples():
+        if int(tup[2]) == group_id:
+            names.add(tup[1])
+
     return list(names)
 
 
@@ -28,14 +41,13 @@ def get_mean_score_of_hw(hw_name: str) -> float:
     return mean_score
 
 
-def get_mean_score_of_hw_in_group(hw_name: str, group_id: str) -> float:
-
+def get_mean_score_of_hw_in_group(hw_name: str, group_id: int) -> float:
     df = get_df_by_hw_name(hw_name)
     sum = 0
     count = 0
 
     for tup in df.itertuples():
-        if str(int(tup[2])) == group_id:
+        if int(tup[2]) == group_id:
             count += 1
             sum += int(tup[4])        
     
@@ -45,29 +57,40 @@ def get_mean_score_of_hw_in_group(hw_name: str, group_id: str) -> float:
     return sum / count
 
 
-def get_student_mark(student_id: int) -> int:
-    sum_scores = hw1_df.iloc[student_id]['Баллы'] + hw2_df.iloc[student_id]['Баллы']
-   
+def get_student_mark(student_name: str) -> int:
+    student_1 = hw_01_df[hw_01_df['Unnamed: 0'] == student_name]
+    student_2 = hw_02_df[hw_02_df['Unnamed: 0'] == student_name]
+    
+    sum_scores = 0
+    if not student_1.empty:
+        sum_scores += student_1['Баллы'].iloc[0]
+    if not student_2.empty:
+        sum_scores += student_2['Баллы'].iloc[0]
+
+    print(sum_scores)
     if sum_scores < 1:
         return 2
-    elif 1 < sum_scores < 30:
+    elif 1 < sum_scores < 50:
         return 3
-    elif sum_scores < 50:
+    elif sum_scores < 80:
         return 4
     return 5
 
+
+def get_mean_group_mark(group_id: int) -> float:
+    sum_marks = 0
+    count = 0
+
+    for student_name in get_group_names(group_id):
+        sum_marks += get_student_mark(student_name)
+        count += 1
+     
+    if count == 0:
+        return 0.0
+    return sum_marks / count
+
+
 if __name__ == '__main__':
-    print(get_mean_score_of_hw_in_group('hw1', '24137'))
+    print(get_mean_group_mark(24137))
 
-    
-
-
-
-
-
-
-
-
-
-
-
+        
--- test.py
+++
@@ -1,7 +0,0 @@
-from parser import get_names
-
-def test_get_names():
-    print(get_names())
-
-if __name__ == '__main__':
-    test_get_names()
